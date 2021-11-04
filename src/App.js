778f4055a21280a7d8e1481809a77f4fadb9b449
th35ly <programador.sly@gmail.com>
2021-11-03 21:40:56 -0300

    conclusão do projeto e funcionalidade de passar as listas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
 import tmdb from './tmdb';
+import Header from './components/header/Header';
 import FeatureMovie from './components/featuremovie/FeatureMovie';
 import MovieRow from './components/movierow/MovieRow'
 
@@ -10,6 +11,7 @@ export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect( ()=> {
     const loadAll = async () => {
@@ -21,17 +23,40 @@ export default () => {
       let originals = list.filter(i=>i.slug === 'originals');
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
       let chosen = originals[0].items.results[randomChosen];
+
+      while (chosen.backdrop_path === null || chosen.genre_ids.length === 0 || chosen.overview === "") {
+        randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+        chosen = originals[0].items.results[randomChosen];
+      }
+      
       let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
-      console.log(chosenInfo);
       setFeatureData(chosenInfo);
     }
 
     loadAll();
   }, []);
 
+  useEffect(()=>{
+    const scrollListner = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListner);
+
+    return () => {
+      window.removeEventListener('scroll',scrollListner);
+    }
+  }, [])
+
 
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
 
       {featureData &&
         <FeatureMovie item={featureData} />
@@ -42,6 +67,18 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por Andre Melo<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pego do site Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 && 
+        <div className="loading">
+          <img alt="carregando" src="https://c.tenor.com/Rfyx9OkRI38AAAAC/netflix-netflix-startup.gif"/>
+        </div>
+      }
     </div>
   );
 }
--- src/components/featuremovie/FeatureMovie.js
+++ src/components/featuremovie/FeatureMovie.js
@@ -10,6 +10,11 @@ export default ({item}) => {
         genres.push( item.genres[i].name );
     }
 
+    let description = item.overview;
+    if (description.length > 250) {
+        description = description.substring(0, 250) + '...'
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -24,9 +29,10 @@ export default ({item}) => {
                         <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
-                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
-                        
+                        <a href={`/watch/${item.id}`} className="featured--watchbutton">► Assistir</a>
+                        <a href={`/list/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
                     </div>
                     <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')} </div>
                 </div>
